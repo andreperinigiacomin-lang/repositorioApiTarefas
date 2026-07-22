0a5da6580dfbdc05b373b568fd2c6e584124d94e
André Perini <andreperinigiacomin@gmail.com>
2026-07-22 10:23:17 -0300

    feat: adiciona os metodos findAll e findById ao controler da api

--- src/controllers/TaskController.ts
+++ src/controllers/TaskController.ts
@@ -8,6 +8,22 @@ class TaskController{
         res.status(201).json(task);
     }
 
+    findAll(res: Response, req: Request): void{
+        const tasks = TaskService.findAll();
+        res.status(200).json(tasks);
+    }
+
+    findById(res: Response, req: Request): void{
+        const id = Number(req.params.id);
+        const task = TaskService.findById(id);
+        if(!task){
+            res.status(404).json({
+                message: "Task não encontrada"});
+            return
+        }
+        res.status(200).json(task);
+    }
+
 }
 
 export default new TaskController();
